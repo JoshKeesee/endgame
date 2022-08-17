194702260cf28700d7a3e43b9f4007718701113d
Joshua <105324950+JoshKeesee@users.noreply.github.com>
2022-08-17 14:21:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,47 @@ function toggleTheme() {
         document.body.style.background = "black";
         document.body.style.color = "white";
         document.getElementById("header").style.color = "black";
+        document.getElementById("container").style.background = "black";
         var content = document.getElementsByClassName("content");
         for (let i = 0; i < content.length; i++) {
             content[i].style.background = "rgba(0, 0, 0, 0.5)";
+            content[i].style.color = "white";
         }
         theme = 1;
     } else {
         document.body.style.background = "white";
         document.body.style.color = "black";
         document.getElementById("header").style.color = "white";
+        document.getElementById("container").style.background = "white";
         var content = document.getElementsByClassName("content");
         for (let i = 0; i < content.length; i++) {
             content[i].style.background = "rgba(255, 255, 255, 0.5)";
+            content[i].style.color = "black";
         }
         theme = 0;
     }
+}
+function big(id) {
+    var element = document.getElementById(id);
+    element.style.position = "fixed";
+    element.style.zIndex = "15";
+    if (theme = 1) {
+        element.style.background = "black";
+        element.style.color = "white";
+    } else {
+        element.style.background = "white";
+        element.style.color = "black";
+    }
+    element.style.top = "13%";
+    element.style.bottom = "0";
+    element.style.left = "0";
+    element.style.right = "0";
+    element.onclick = function() { normal(element.id) };
+}
+function normal(id) {
+    var element = document.getElementById(id);
+    element.style.position = "";
+    element.onclick = function() { big(element.id) };
 }
 window.addEventListener("scroll", function () {
     var background = document.getElementsByClassName("background");
